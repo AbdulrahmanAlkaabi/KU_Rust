601e3a353300fd7cfb3ef8a3000621092f08e5a7
AbdulrahmanAlkaabi <662907688aths@gmail.com>
2025-02-04 11:43:35 +0400

    Lab 2

--- rustlings/exercises/08_enums/enums3.rs
+++ rustlings/exercises/08_enums/enums3.rs
@@ -46,11 +46,19 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    
 }
 
 #[cfg(test)]
