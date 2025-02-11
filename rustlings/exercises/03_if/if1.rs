a3777e19167d342b6b63a9ce7fcc31df632e5215
AbdulrahmanAlkaabi <662907688aths@gmail.com>
2025-02-11 10:27:15 +0400

    Lab 3

--- rustlings/exercises/03_if/if1.rs
+++ rustlings/exercises/03_if/if1.rs
@@ -4,6 +4,14 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    if a<=b {
+        //return b;
+        b
+    } else {
+        //return a;
+        a
+    }
 }
 
 fn main() {
