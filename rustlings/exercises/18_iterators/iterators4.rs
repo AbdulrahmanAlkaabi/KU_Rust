c075799e0da6cab6a6f7b6cb5cc06bc0cdf7e899
AbdulrahmanAlkaabi <662907688aths@gmail.com>
2025-04-08 09:48:17 +0400

    Lab 9

--- rustlings/exercises/18_iterators/iterators4.rs
+++ rustlings/exercises/18_iterators/iterators4.rs
@@ -10,6 +10,12 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    let mut x = 1;
+    for i in 1..=num {
+        x *= i;
+    }
+    x
 }
 
 fn main() {
